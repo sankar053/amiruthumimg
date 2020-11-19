8d2b68ee25912e96de7af0c9744a522d3dd116ab
sankar053 <sankaraasn@gmail.com>
2020-11-19 17:35:25 +0530

    Initial commit

---
+++ src/main/java/com/iii/techyon/CoronaMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iii.techyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iii/techyon/CoronaMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iii.techyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
